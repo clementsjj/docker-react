f1598408cf731d0054b49a10e2e5ccb9caaa61e9
JJ Clements <clementsjj@gmail.com>
2020-02-06 14:51:31 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hey Wut Up Homies</p>
+        <p>Hot loading baby!!</p>
+        <p>WOOHOO</p>
       </header>
     </div>
   );
